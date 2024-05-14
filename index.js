eef3a3296c04c229551d3c6f1011d48fe4450912
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-15 00:42:11 +0200

    all-set

--- index.js
+++ index.js
@@ -19,12 +19,38 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const isInvalidDate = (date) => isNaN(date.getTime());
 
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid date"})
+    return;
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+app.get("/api", (req, res)=> {
+  res.json({
+    unix: new Date().getTime(),
+    utx: new Date().toUTCString()
+  })
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
